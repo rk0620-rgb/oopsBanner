c444b46ab425f97dec8127857ad43e870e20a51c
rk0620-rgb <rk0620@srmist.edu.in>
2026-02-17 09:33:22 +0530

    initial commit of usecase2

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,3 +1,17 @@
 public class OOPSBannerApp{
 public static void main(String[] args) {
-System.out.println("OOPS");}}
+System.out.println("   ***   " + " "+"    ***   " + " " + "  ******    "+" "+"   *****");
+System.out.println(" **   ** " + " "+"  **   **" + " " + "   **    "+" "+"    *********");
+System.out.println(" **   **" + " "+"   **   **" + " " + "   **    ** "+" "+"  **");
+System.out.println("**     **" + " "+" **     **" + " " + "  **     **"+" "+" **");
+System.out.println("**     **" + " "+" **     **" + " " + "  **    **"+" "+"   **");
+System.out.println("**     **" + " "+" **     **" + " " + "  ******   "+" "+"    ***");
+System.out.println("**     **" + " "+" **     **" + " " + "  **    "+" "+"          **");
+System.out.println("**     **" + " "+" **     **" + " " + "  **    "+" "+"           **");
+System.out.println(" **   ** " + " "+"  **   **" + " " + "   **    "+" "+"          **");
+System.out.println(" **   ** " + " "+"  **   **" + " " + "   **    "+" "+"    *********");
+System.out.println("   ***   " + " "+"    ***   " + " " + "  **    "+" "+"     *****");
+
+
+}
+}
